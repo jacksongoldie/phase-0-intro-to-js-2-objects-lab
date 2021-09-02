3c9d1b799afb8384467997fa773505ad32ba7b17
Goldie <goldiejackson25@yahoo.com>
2021-09-02 13:02:35 -0500

    commit tree 7c0c665fe6d4fb54b06d22a4aea673af2615a440

--- index.js
+++ index.js
@@ -4,26 +4,75 @@ const employee = {
     streetAddress: "404 W Mosier",
 }
 
+//9-2-21 
+function updateEmployeeWithKeyAndValue(employeeObject, key, value){
+    return {
+        ...employeeObject,
+        [key] : value,
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObject, key, value){
+    employeeObject[key] = value;
+    return employeeObject;
+}
+
+
+//Is this working? It passes, but I'm not sure it's deleting anything
+function deleteFromEmployeeByKey(employeeObject, key){
+    const newObject = {...employeeObject};
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObject, key){
+    delete employeeObject[key];
+    return employeeObject;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Update employee nondestructive
-function updateEmployeeWithKeyAndValue(employee, key, value){
+/* function updateEmployeeWithKeyAndValue(employee, key, value){
     return{
         ...employee,
         [key]: value,
     }
-}
+} */
 
 //Update employee info destructive
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+/* function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
     employee[key] = value;
     return employee;
-}
+} */
 
 //Delete key from employee nondestructive
-function deleteFromEmployeeByKey(employee){
+/* function deleteFromEmployeeByKey(employee){
     const newEmployee = {...employee};
     delete newEmployee.name;
     return newEmployee;
-}
+} */
 
     /*/Other code found - need to look up object assign
     function deleteFromEmployeeByKey(obj, key, value) {
@@ -32,7 +81,7 @@ function deleteFromEmployeeByKey(employee){
     */
 
 //Delete key from employee destructive
-function destructivelyDeleteFromEmployeeByKey(){
+/* function destructivelyDeleteFromEmployeeByKey(){
     delete employee.name;
     return employee;
-}
+} */
